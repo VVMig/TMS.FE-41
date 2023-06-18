69a049bec3c98eae5039384d286116864654838b
unknown <ostrovskiy.98@mail.ru>
2023-06-18 20:51:19 +0300

    hw

--- react-blog/src/layout/index.tsx
+++ react-blog/src/layout/index.tsx
@@ -1,11 +1,19 @@
 import { Link, Outlet } from "react-router-dom";
 import { Routes } from "../constants/Routes";
+import './header.css';
 
 export const Root = () => {
   return (
     <>
-      <header>
-        <Link to={Routes.Register}>Register</Link>
+      <header className="header">
+        <div>
+          <Link to={Routes.Register} className="register">Register</Link>
+        </div>
+        <div>
+          <form>
+            <input className="search" type="text" placeholder="Serch..." />
+          </form>
+        </div>
       </header>
       <div>
         <Outlet />
--- react-blog/src/store/index.ts
+++ react-blog/src/store/index.ts
@@ -7,3 +7,6 @@ export const rootReducer = combineReducers({
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
