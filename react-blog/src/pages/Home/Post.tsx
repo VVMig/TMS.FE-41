d2911c839e59306fdac8273968a5cb6d52e99962
unknown <ostrovskiy.98@mail.ru>
2023-06-29 00:55:29 +0300

    add some styles

--- react-blog/src/pages/Home/Post.tsx
+++ react-blog/src/pages/Home/Post.tsx
@@ -6,9 +6,15 @@ import {
   CardMedia,
   CardContent,
   Typography,
+  CardActions,
+  Checkbox,
 } from "@mui/material";
 import { red } from "@mui/material/colors";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbDownAltIcon from "@mui/icons-material/ThumbDownAlt";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
 
 interface IProps {
   title: string;
@@ -16,28 +22,41 @@ interface IProps {
   text: string;
 }
 
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
 export const Post = ({ title, src, text }: IProps) => {
   return (
     <Card sx={{ maxWidth: 345, marginTop: 12 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={title}
-      />
       <CardMedia component="img" height="194" image={src} alt="Paella dish" />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
           {text}
         </Typography>
       </CardContent>
+      <CardActions sx={{display: "flex", alignItems: "center", justifyContent: "space-between"}}>
+        <div>
+          <Checkbox
+            {...label}
+            icon={<ThumbUpIcon />}
+            checkedIcon={<ThumbUpIcon />}
+          />
+          <Checkbox
+            {...label}
+            icon={<ThumbDownAltIcon />}
+            checkedIcon={<ThumbDownAltIcon />}
+          />
+        </div>
+        <div>
+          <Checkbox
+            {...label}
+            icon={<BookmarkBorderIcon />}
+            checkedIcon={<BookmarkIcon />}
+          />
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </CardActions>
     </Card>
   );
 };
