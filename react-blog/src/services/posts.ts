292599353d8253492025614e520b9b1e719196ed
akavaliova <kovaleva89alena@gmail.com>
2023-07-04 13:52:46 +0200

    changed file react-blog_src_services_posts.ts for the lesson-13

--- react-blog/src/services/posts.ts
+++ react-blog/src/services/posts.ts
@@ -1,12 +1,11 @@
 import { api } from "./configs/http";
 class PostsService {
   getAll() {
-    const data = api.get("/blog/posts/",{
+    return api.get("/blog/posts/", {
       params:{
-        limit: 15
+        limit: 100,
       },
     });
-      return data
     }
   }
   export const postsService = new PostsService();
